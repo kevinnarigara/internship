5e7de1c5f42fe1ede6e39caee432569704e3d23e
kevinnarigara <85152397+kevinnarigara@users.noreply.github.com>
2021-06-02 14:46:37 +0530

    Add files via upload

---
+++ Day 5/q5.py
@@ -0,0 +1,11 @@
+
+class employee():
+    def __init__(self):
+        name=input("Enter a name :")
+        desigation = input("Enter a position :")
+class subclass(employee):
+    def field(self):
+        salary=int(input("Enter a salary :"))
+
+c=subclass()
+c.field()
